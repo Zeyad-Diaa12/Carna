b7f153aa870e976fa09bb82f59a12aca6203afb3
Zeyad-Diaa12 <100387591+Zeyad-Diaa12@users.noreply.github.com>
2022-04-21 22:23:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let active = document.querySelector('.nav').querySelectorAll('li');
 
-console.log(active)
+let element=document.body;
+
+let header=document.querySelector('.header_light');
+
 active.forEach(element =>{
     element.addEventListener('click',function(){
         active.forEach(nav=>nav.classList.remove('active'))
@@ -9,5 +12,16 @@ active.forEach(element =>{
     })
 })
 
+function dark_mode(){
+    element.classList.add("dark_mode");
+    header.classList.remove("header_light");
+    header.classList.add("header_dark");
+}
+
+function light_mode(){
+    element.classList.remove("dark_mode");
+    header.classList.remove("header_dark");
+    header.classList.add("header_light");
+}
 
 
