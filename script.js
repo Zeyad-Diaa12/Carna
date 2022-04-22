460d25e13260bba2d9b44539a80b35ac7a57e552
Zeyad-Diaa12 <100387591+Zeyad-Diaa12@users.noreply.github.com>
2022-04-22 15:07:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,47 +6,65 @@ let github=document.querySelectorAll('.github-light');
 let anchor=document.querySelector('.nav').querySelectorAll('a');
 
 
+// Function for active anchor
 active.forEach(element =>{
+    
     element.addEventListener('click',function(){
         active.forEach(nav=>nav.classList.remove('active'))
         this.classList.add('active')
     })
+
 })
 
+
+// Function Dark mode
 function dark_mode(){
+
     element.classList.add("dark_mode");
     header.classList.remove("header_light");
     header.classList.add("header_dark");
+    
     for(let i =0;i<member_card.length;i++){
         member_card[i].classList.remove("member-light");
         member_card[i].classList.add("member-dark");
         github[i].classList.remove("github-light");
         github[i].classList.add("github-dark");
     }
+
     for(let i=0;i<anchor.length;i++){
         anchor[i].classList.remove("light-mode");
         anchor[i].classList.add("dark-mode");
     }
+
 }
 
+
+// Function Light mode
 function light_mode(){
+
     element.classList.remove("dark_mode");
     header.classList.remove("header_dark");
     header.classList.add("header_light");
+
     for(let i =0;i<member_card.length;i++){
         member_card[i].classList.remove("member-dark");
         member_card[i].classList.add("member-light");
         github[i].classList.remove("github-dark");
         github[i].classList.add("github-light");
     }
+
     for(let i=0;i<anchor.length;i++){
         anchor[i].classList.remove("dark-mode");
         anchor[i].classList.add("light-mode");
     }
+
 }
 
+
+// Function to send email
 function sendMail()
 {
+
     let body=document.getElementById('#body_message');
     let subject=document.querySelector('.subject_message');
     
@@ -56,4 +74,5 @@ function sendMail()
     // Clear data after sending email
     body.value='';
     subject.value='';
+
 }
